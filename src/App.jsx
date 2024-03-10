0f5a76d95ec1dabcae613bd0d8160f551437f3cc
Sunil Kumar <sunilkumar24.dev@gmail.com>
2024-03-10 12:35:59 +0500

    buttonscolorsfixedafterrestart

--- src/App.jsx
+++ src/App.jsx
@@ -115,6 +115,7 @@ const App = () => {
     setFeedback(initialGrid);
     setGrid(initialGrid);
     setShowModal(false);
+    setLetterFeedback({});
   };
 
   const getFeedbackClass = (feedbackValue) => {
